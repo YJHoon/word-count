62a4db8aff497619cd42833e9564a48271f59826
Yeon JeHoon <dusgns01@naver.com>
2019-06-04 15:15:22 +0000

    first commit

--- first_project/settings.py
+++ first_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'krt)yq-8yu-06+c8r3je((up_6ke6n_s(de^r4@e7*rhywto+^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wordcount.apps.WordcountConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static'),
+]
